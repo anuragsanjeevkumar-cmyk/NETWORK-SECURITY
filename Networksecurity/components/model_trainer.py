30c82aaa35ee633f60b39a7d5340e19c0dcd0d4f
anuragsanjeevkumar-cmyk <anuragsanjeevkumar@gmail.com>
2026-06-17 18:52:56 +0530

    done

--- Networksecurity/components/model_trainer.py
+++ Networksecurity/components/model_trainer.py
@@ -27,11 +27,11 @@ import mlflow
 from urllib.parse import urlparse
 
 import dagshub
-#dagshub.init(repo_owner='krishnaik06', repo_name='networksecurity', mlflow=True)
+dagshub.init(repo_owner='anuragsanjeevkumar-cmyk', repo_name='NETWORK-SECURITY', mlflow=True)
 
-os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/krishnaik06/networksecurity.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"]="krishnaik06"
-os.environ["MLFLOW_TRACKING_PASSWORD"]="7104284f1bb44ece21e0e2adb4e36a250ae3251f"
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/anuragsanjeevkumar-cmyk/NETWORK-SECURITY.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="anuragsanjeevkumar-cmyk"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="1790dd0dd1da04e82a7a445909f18ccdef501139"
 
 
 
@@ -46,7 +46,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        mlflow.set_registry_uri("https://dagshub.com/anuragsanjeevkumar-cmyk/NETWORK-SECURITY.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
