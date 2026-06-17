27223802f254cdecc3e287b25a1c161c27e8c1c5
anuragsanjeevkumar-cmyk <anuragsanjeevkumar@gmail.com>
2026-06-17 17:29:01 +0530

    trained evaluation and mlflow commited

--- main.py
+++ main.py
@@ -3,68 +3,45 @@ from Networksecurity.components.data_validation import DataValidation
 from Networksecurity.components.data_transformation import DataTransformation
 from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
-from Networksecurity.entity.config_entity import (
-    DataIngestionConfig,
-    DataValidationConfig,
-    TrainingPipelineConfig,
-    DataTransformationConfig
-)
+from Networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from Networksecurity.entity.config_entity import TrainingPipelineConfig
+
+from Networksecurity.components.model_trainer import ModelTrainer
+from Networksecurity.entity.config_entity import ModelTrainerConfig
+ 
 
 import sys
 
-if __name__ == "__main__":
+if __name__=='__main__':
     try:
-        # Training Pipeline Config
-        training_pipeline_config = TrainingPipelineConfig()
-
-        # Data Ingestion Config
-        data_ingestion_config = DataIngestionConfig(
-            training_pipeline_config=training_pipeline_config
-        )
-
-        # Data Ingestion
-        data_ingestion = DataIngestion(
-            data_ingestion_config=data_ingestion_config
-        )
-
-        logging.info("Initiating Data Ingestion")
-
-        data_ingestion_artifact = (
-            data_ingestion.initiate_data_ingestion()
-        )
-
-        logging.info("Data Ingestion Completed")
-
-        print(data_ingestion_artifact)
-
-        # Data Validation Config
-        data_validation_config = DataValidationConfig(
-            training_pipeline_config=training_pipeline_config
-        )
-
-        # Data Validation
-        data_validation = DataValidation(
-            data_ingestion_artifact=data_ingestion_artifact,
-            data_validation_config=data_validation_config
-        )
-
-        logging.info("Initiating Data Validation")
-
-        data_validation_artifact = (
-            data_validation.initiate_data_validation()
-        )
-
-        logging.info("Data Validation Completed")
-
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
         print(data_validation_artifact)
-        data_transformation_config = DataTransformationConfig(
-            training_pipeline_config=training_pipeline_config
-                                    )
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
-        logging.info("Data Transformation completed")
+        logging.info("data Transformation completed")
 
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
+        
+        
+        
     except Exception as e:
-        logging.error(str(e))
-        raise NetworkSecurityException(e, sys)
+           raise NetworkSecurityException(e,sys)
